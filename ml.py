94663d587f82dd2bde2b6c93c7218b95e86a631e
Udayv56 <126442827+Udayv56@users.noreply.github.com>
2023-03-23 03:59:49 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,24 @@
+#import pandas as pd
+#from sklearn.model_selection import train_test_split
+#from sklearn.linear_model import LinearRegression
+
+# Load the data into a pandas DataFrame
+#data = pd.read_csv('data.csv')
+
+# Split the data into independent variables (X) and dependent variables (y)
+#X = data[['feature1', 'feature2', 'feature3']]
+#y = data['target']
+
+# Split the data into training and testing sets
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Create the linear regression model
+
+#model = LinearRegression()
+
+# Train the model on the training data
+#model.fit(X_train, y_train)
+
+# Evaluate the model on the testing data
+#accuracy = model.score(X_test, y_test)
+#print("Accuracy:", accuracy)
